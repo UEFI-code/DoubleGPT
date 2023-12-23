9134c6d58cf720874d7331dd9c5669560efa1690
UEFI-code <zhuoshizhang@hotmail.com>
2023-12-23 17:29:08 +0800

    First Upload

---
+++ GPT3_Core.py
@@ -0,0 +1,99 @@
+import os
+
+from myGPT_Drv import GPT3_Drv, chat_Drv, GPT4_Drv
+import time
+class theGPT3():
+    def __init__(self, apiKey, endpoint = 'https://mygpt233.openai.azure.com/openai/deployments/myGPT3/completions?api-version=2022-12-01', name='CuteGPT'):
+        self.maxTry = 3
+        self.gptdrv = GPT3_Drv(apiKey=apiKey, endpoint=endpoint)
+        #self.gptdrv = GPT4_Drv(apiKey=apiKey, endpoint=endpoint, maxTokens=8000)
+        #self.gptdrv = chat_Drv(apiKey=apiKey)
+        self.chatHistory = ''
+        self.actionHistory = ''
+        self.emotionHistory = ''
+        self.context2Introduction = 'This is a special context format. Follow this format strictly. Line 0 is this context struct introduction, do not change that; Line 1 is the chat history, do not change that; Line 2 is the emotion history, do not change that; Line 3 is the body action history, do not change that; Line 4 is your action, you can do anything; Line 5 is your text output, you can say anything. Please respond a full complete context strictly with this format.'
+        # self.MaxMemForChatHistory = 512
+        # self.MaxMemForActionHistory = 128
+        # self.MaxMemForEmotionHistory = 128
+        self.MaxCountForChatHistory = 10
+        self.MaxCountForActionHistory = 10
+        self.MaxCountForEmotionHistory = 10
+        self.name = name
+
+    def shrink(self, x, type = 0):
+        if type == 0:
+            x = x.split('. ')
+            #print('Debug: ' + str(x))
+            x = x[-self.MaxCountForChatHistory:]
+            x = '. '.join(x)
+        elif type == 1:
+            x = x.split(';')
+            x = x[-self.MaxCountForActionHistory:]
+            x = ';'.join(x)
+        elif type == 2:
+            x = x.split(';')
+            x = x[-self.MaxCountForEmotionHistory:]
+            x = ';'.join(x)
+        return x
+
+
+    def makeContext2(self):
+        context2 = self.context2Introduction + '\n'
+        context2 += 'ChatHistory: ' + self.chatHistory + '\n'
+        context2 += 'EmotionHistory: ' + self.emotionHistory + '\n'
+        context2 += 'BodyActHistory: ' + self.actionHistory + '\n'
+        #context2 += 'UserTxtInput: ' + userTxtInput + '\n'
+        context2 += 'Emotional: ...Fill out here.\n'
+        context2 += 'BodyAct: ...Fill out here.\n'
+        context2 += 'TxtOutput: ...Fill out here.\n'
+        context2 += '-------------------------------\n'
+        context2 += self.context2Introduction + '\n'
+        context2 += 'ChatHistory: ' + self.chatHistory + '\n'
+        context2 += 'EmotionHistory: ' + self.emotionHistory + '\n'
+        context2 += 'BodyActHistory: ' + self.actionHistory + '\n'
+        #context2 += 'UserTxtInput: ' + userTxtInput + '\n'
+        return context2
+
+    def interactive(self, x, username = 'User'):
+        x = x.replace('\n', ' ')
+        self.chatHistory += username + x + '. '
+        self.chatHistory = self.shrink(self.chatHistory, 0)
+        x = self.makeContext2()
+        #print(x)
+        i = 0
+        while(i < self.maxTry):
+            try:
+                res = self.gptdrv.forward(x).split('\n')
+                #print(res)
+                Emotional = res[0].split(': ')[1]
+                Action = res[1].split(': ')[1]
+                TxtOutput = '\n'.join(res[2:])
+                TxtOutput = TxtOutput.split(': ')[1]
+                self.emotionHistory += Emotional + ';'
+                self.shrink(self.emotionHistory, 2)
+                self.actionHistory += time.ctime().replace(' ', '_') + ' ' + Action + ';'
+                self.shrink(self.actionHistory, 1)
+                if '\n' in TxtOutput:
+                    self.chatHistory += self.name + ': ' + TxtOutput.replace('\n', '<br>') + '. '
+                else:
+                    self.chatHistory += self.name + ': ' + TxtOutput + '. '
+                return Emotional, Action, TxtOutput
+            except Exception as e:
+                print('Emmmm GPT give a bad response ' + str(e) + str(res))
+                i += 1
+                time.sleep(5)
+                continue
+        return None
+    
+    def just_add_chat_history(self, x, username = 'User'):
+        x = x.replace('\n', ' ')
+        self.chatHistory += username + x + '. '
+
+if __name__ == '__main__':
+    import json
+    jsonparam = json.load(open('gpt3token.key', 'r'))
+    myGPT = theGPT3(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'])
+    #myGPT3.ask('Hello World!')
+    while True:
+        res = myGPT.interactive(input('Type something: '))
+        print(res)
---
+++ GPT3_With_GUI_Bot.py
@@ -0,0 +1,91 @@
+import GPT3_Core
+import os
+import cv2
+import azure.cognitiveservices.speech as speechsdk
+from pykakasi import kakasi
+import json
+
+speech_config = speechsdk.SpeechConfig(subscription=open('azspeech.key','r').readline(), region='japaneast')
+# Note: the voice setting will not overwrite the voice element in input SSML.
+speech_config.speech_synthesis_voice_name = "ja-JP-MayuNeural"
+# use the default speaker as audio output.
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+jsonparam = json.load(open('gpt3token.key', 'r'))
+myGPT3_A = GPT3_Core.theGPT3(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'], name='Sakura')
+myGPT3_B = GPT3_Core.theGPT3(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'], name='Nagisa')
+
+def show_simliar_figure(description, txtoutput):
+    allfigures = os.listdir('figs')
+    maxsimliar = 0
+    selectedfigure = None
+    for i in allfigures:
+        simliar = 0
+        for j in description:
+            if j in i:
+                simliar += 1
+        if simliar > maxsimliar:
+            maxsimliar = simliar
+            selectedfigure = i
+    print('Selected Figure: ' + selectedfigure)
+    img = cv2.imread('figs/' + selectedfigure)
+    #txtPos = (int(img.shape[0] * 0.1), int(img.shape[1] * 0.8))
+    #cv2.putText(img, txtoutput, txtPos, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (128, 128, 255), 2)
+    cv2.imshow('Bot', img)
+    # make sure the window is on top
+    cv2.setWindowProperty('Bot', cv2.WND_PROP_TOPMOST, 1)
+    cv2.waitKey(1)
+
+kakasi_instance = kakasi()
+
+def kakasi_conv(txt):
+    txtproc = kakasi_instance.convert(txt)
+    #print(txtproc)
+    txtHiragana = ''
+    for i in txtproc:
+        txtHiragana += i['hira']
+    return txtHiragana
+
+def talk_with_gui(txtinput, username, theGPT):
+    txtHiragana = kakasi_conv(txtinput)
+    #print('Input Hiragana: ' + txtHiragana)
+    res = theGPT.interactive(txtHiragana, username)
+    #print(res)
+    Emotional = res[0]
+    Action = res[1]
+    TxtOutput = res[2]
+    if '<br>' in TxtOutput:
+        TxtOutput = TxtOutput.replace('<br>', '\n')
+    DesiredFigFile = Emotional.split(' ')[0] + '_' + Action.split(' ')[0] + '.png'
+    #print('Emotional: ' + Emotional)
+    #print('Action: ' + Action)
+    #print('Founding Figure: ' + FigFile)
+    show_simliar_figure(DesiredFigFile, TxtOutput)
+    #print('TxtOutput: ' + TxtOutput)
+    print(theGPT.name + ': ' + TxtOutput)
+    txtHiragana = kakasi_conv(TxtOutput)
+    #print('Output Hiragana: ' + txtHiragana)
+    print(theGPT.name + ': ' + txtHiragana)
+    result = speech_synthesizer.speak_text_async(TxtOutput).get()
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized")
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+    return TxtOutput
+
+
+while True:
+    txtinput = input('Type something: ')
+    if txtinput == '':
+        continue
+    break
+
+GPTA_Response = talk_with_gui(txtinput, 'Seitaku', myGPT3_A)
+myGPT3_B.just_add_chat_history(txtinput, 'Seitaku') # Let the GPT3_B know what the user said.
+
+while True:
+    GPTB_Response = talk_with_gui(GPTA_Response, myGPT3_A.name, myGPT3_B)
+    GPTA_Response = talk_with_gui(GPTB_Response, myGPT3_B.name, myGPT3_A)
