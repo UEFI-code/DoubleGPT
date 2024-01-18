483ccfcf2deb22b30658d2659171ea1658f30606
UEFI-code <zhuoshizhang@hotmail.com>
2024-01-18 11:51:55 +0800

    Bug Fix

--- myGPT_Drv.py
+++ myGPT_Drv.py
@@ -28,7 +28,7 @@ class GPT3_Drv:
         return response.json()['choices'][0]['text']
 
 class chat_Drv:
-    def __init__(self, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myGPTChat_3_5/chat/completions?api-version=2023-03-15-preview", apiKey="233333"):
+    def __init__(self, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myGPTChat_3_5/chat/completions?api-version=2023-03-15-preview", apiKey="233333", maxTokens = 4000):
         self.endpoint = endpoint
         self.header = {
             "Content-Type": "application/json",
@@ -44,11 +44,11 @@ class chat_Drv:
             "presence_penalty": 0,
             "stop": None
         }
+        self.maxTokens = maxTokens
     
     def forward(self, x = 'Hello World'):
-        x = x[-3800:]
         self.body['messages'] = self.messages + [{"role":"user","content":x}]
-        self.body['max_tokens'] = 3900 - len(x)
+        self.body['max_tokens'] = self.maxTokens - len(x)
         if (self.body['max_tokens'] < 0):
             raise Exception('The input text is too long.')
         response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
@@ -74,7 +74,6 @@ class GPT4_Drv:
         self.maxTokens = maxTokens
 
     def forward(self, x = 'Hello World'):
-        x = x[-(self.maxTokens - 100):]
         self.body['messages'] = self.messages + [{"role":"user","content":x}]
         self.body['max_tokens'] = self.maxTokens - len(x)
         if (self.body['max_tokens'] < 0):
@@ -86,12 +85,6 @@ class GPT4_Drv:
             return 'Net Error ' + str(response)
     
 if __name__ == '__main__':
-    #gpt3 = GPT3_Drv(apiKey=open('azgpt3.key', 'r').readline())
-    #print(gpt3.forward('Hello World'))
-    # chatgpt = GPT4_Drv(apiKey=open('azgpt3.key', 'r').readline())
-    # print(chatgpt.forward('Hello World'))
-    # chatgpt = chat_Drv(apiKey=open('azgpt3.key', 'r').readline())
-    # print(chatgpt.forward('Hello World'))
     import json
     jsonparam = json.load(open('gpt3token.key', 'r'))
     gpt = GPT3_Drv(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'])
